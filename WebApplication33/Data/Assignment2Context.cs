d29ba9bff9d778f78a05e6e27e7471d82814fb76
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-20 17:38:50 +0530

    Add project files.

---
+++ WebApplication33/Data/Assignment2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Assignment2.Model;
+
+namespace Assignment2.Data
+{
+    public class Assignment2Context : DbContext
+    {
+        public Assignment2Context (DbContextOptions<Assignment2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Assignment2.Model.Student> Student { get; set; }
+    }
+}
